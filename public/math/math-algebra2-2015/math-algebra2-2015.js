9d180d95fb148b1dd0d7a918cc50f75cc343a2eb
vivien <vivien.c.berg@gmail.com>
2025-06-26 23:58:53 -0400

    quick local commit before i do anything more

---
+++ public/math/math-algebra2-2015/math-algebra2-2015.js
@@ -0,0 +1,12 @@
+// Grade 3 Math Questions
+
+const sampleQuestions = [
+ {
+ id: '1',
+ text: 'Which expression is equivalent to <math><msqrt><mi>20x¹⁶y²⁵</mi></mqsrt></math> for positive x and y values?',
+ type: 'multiple-choice',
+ options: ['15 - 9 = __', '15 × 9 = __', '15 ÷ 9 = __', '15 + 9 = __'],
+ correctAnswer: 0,
+ explanation: 'Addition and subtraction are inverse operations, so 9 + 6 = 15 helps solve 15 - 9 = 6.'
+ },
+];
